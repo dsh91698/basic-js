579dfe07ddf5cc9d19980f428fcef5d8066880d5
dsh91698 <56885229+dsh91698@users.noreply.github.com>
2022-05-02 21:35:51 +0400

    feat: cats ears counted

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = '';
+  let arr = [];
+  for (let i = 0; i < members.length; i++) { // check for name type
+    if (typeof members[i] === 'string') {
+      arr.push(members[i].charAt(0));
+    }
+  }
 }
 
 module.exports = {
